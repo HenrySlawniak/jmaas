062c76c3d352e7933bc5a66ce9d8456145214e2c
Henry Slawniak <meggawatts@gmail.com>
2017-03-15 19:05:58 -0500

    Add the best domain

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ import (
 const version = "1.1.0"
 
 var devMode = flag.Bool("dev", false, "Puts the server in developer mode, will bind to :34265 and will not autocert")
-var domains = flag.String("domain", "jmaas.servercentralbathroomselfies.com,baklavodka.com", "A comma-seperaated list of domains to get a certificate for.")
+var domains = flag.String("domain", "jmaas.servercentralbathroomselfies.com,baklavodka.com,angrymills.net", "A comma-seperaated list of domains to get a certificate for.")
 var client = &http.Client{}
 var level = 0
 
